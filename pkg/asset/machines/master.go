96ba67fd2b88e46d1502876639dde22e3bf73a17
Paul Reinking <paul.reinking@users.noreply.github.com>
2020-05-02 11:54:14 +0200

    fixed sizes

--- pkg/asset/machines/master.go
+++ pkg/asset/machines/master.go
@@ -238,7 +238,7 @@ func (m *Master) Generate(dependencies asset.Parents) error {
 	case azuretypes.Name:
 		mpool := defaultAzureMachinePoolPlatform()
 		mpool.InstanceType = azuredefaults.ControlPlaneInstanceType(installConfig.Config.Platform.Azure.Region)
-		mpool.OSDisk.DiskSizeGB = 1024
+		mpool.OSDisk.DiskSizeGB = 512
 		mpool.Set(ic.Platform.Azure.DefaultMachinePlatform)
 		mpool.Set(pool.Platform.Azure)
 		if len(mpool.Zones) == 0 {
